33353c922f606955a76c4a3cb3c7e3d94f3e30a7
Isaac Bowen <irbowen@umich.edu>
2016-11-16 05:00:02 -0500

    fix to email

--- util.py
+++ util.py
@@ -7,7 +7,7 @@ for line in lines:
     line = line.strip('\n')
     words = line.split(',')
     unique_name = words[0]
-    email = unique_name + "@umich.edu"
+    email = unique_name # + "@umich.edu"
     # Ignore those on the wall of shame less than a week
     if (int(words[3]) < 7):
         continue
